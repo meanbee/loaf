a2bbcc8f5c517af7b84b00f8a0620f1d20d31aee
Martin Shaw <martin.shaw@meanbee.com>
2012-02-10 13:10:05 +0000

    All done except the web service stuff

--- chew.py
+++ chew.py
@@ -3,6 +3,10 @@ import argparse
 import os
 import shutil
 
+# Constants
+KITCHEN = '/usr/local/Kitchen'
+BIN = '/usr/local/bin'
+
 def parseArgs():
     parser = argparse.ArgumentParser(description='Copy a file from base/default into a user specified theme and commit the file.')
     parser.add_argument('Package', metavar='package', 
@@ -15,10 +19,51 @@ def parseArgs():
 def getRepoUrl(package):
     return 'git@codebasehq.com:meanbee/general/dac.git'
 
-# Commence the running!
+def goToKitchen():
+    # See if the Kitchen exists...
+    if not os.path.isdir(KITCHEN):
+        # It doesn't, lets make it
+        os.makedirs(KITCHEN)
+
+    # Change directory into the kitchen
+    os.chdir(KITCHEN)
+
+def cloneRepository(repo_url):
+    os.system('git clone ' + repo_url)
+
+def makeSymlink(package):
+    # See if the Kitchen exists...
+    if not os.path.isdir(BIN):
+        # It doesn't, lets make it
+        os.makedirs(BIN)
+
+    # Change directory into the bin folder
+    os.chdir(BIN)
+        
+    #Make the symlink
+    src = KITCHEN + '/' + package + '/' + package
+    dest = BIN + '/' + package
+    os.system('ln -s ' + src + ' ' + dest)
+
+    
+    
+
+# !!! SCRIPT STARTS HERE !!!
+
+# Let's get some arguments
 args = parseArgs()
 package = args.Package
 
 # Get the git repository associated with this package
 repo_url = getRepoUrl(package)
 
+# Go to the Kitchen, it's time to bake!
+goToKitchen()
+
+# Let's get the repository
+cloneRepository(repo_url)
+
+# Cool, let's make a symlink to the bin folder
+makeSymlink(package)
+
+# Done.
